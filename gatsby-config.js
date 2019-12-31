576ab97b1c3cb46f6d1e42734a88b0b07603052f
shgtkshruch <shgtk.shruch@gmail.com>
2019-12-31 22:53:02 +0900

    npm install --save gatsby-transformer-remark
    https://www.gatsbyjs.org/packages/gatsby-transformer-remark/

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,21 @@ module.exports = {
         name: `src`,
         path: `${__dirname}/src/`,
       },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
