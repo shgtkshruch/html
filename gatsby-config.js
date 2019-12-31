825a73dbece00fbda650aa21abfcca4addde9016
shgtkshruch <shgtk.shruch@gmail.com>
2019-12-31 22:48:14 +0900

    npm install --save gatsby-source-filesystem
    https://www.gatsbyjs.org/packages/gatsby-source-filesystem/

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,13 @@ module.exports = {
       },
     },
     `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
