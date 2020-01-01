62e9de24e7ba32cea99c21a3225c6e9adc587b40
shgtkshruch <shgtk.shruch@gmail.com>
2020-01-01 21:42:31 +0900

    Add element list link to top page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,7 @@ import { Link } from "gatsby"
 
 import { useStaticQuery, graphql } from "gatsby"
 
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => {
@@ -22,13 +20,8 @@ const IndexPage = () => {
   return (
     <Layout title={ data.site.siteMetadata.title }>
       <SEO title="Home" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
-      <Link to="/page-2/">Go to page 2</Link>
+      <h1>HTML Element List</h1>
+      <Link to="/element/">Element List</Link>
     </Layout>
   )
 }
