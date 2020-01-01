e701f41e1d23f3e1c5c650b9fe6bffd092c227bb
shgtkshruch <shgtk.shruch@gmail.com>
2020-01-01 09:59:02 +0900

    Render initial element page

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,38 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require(`path`)
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  const result = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+            }
+          }
+        }
+      }
+    }
+  `)
+  if (result.errors) {
+    console.error(result.errors)
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    console.dir(`node id is ${node.id}`);
+    createPage({
+      path: path.join('element', 'html'),
+      component: path.resolve(__dirname, "./src/templates/element.js"),
+      context: {
+        id: node.id,
+      }
+    })
+  })
+}
