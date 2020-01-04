874ffaa7e01fb753a8e376dbd50653f2d7fa9d77
shgtkshruch <shgtk.shruch@gmail.com>
2020-01-04 19:39:20 +0900

    Add MDN link to each element page

--- src/templates/element.js
+++ src/templates/element.js
@@ -4,16 +4,20 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const mdnUrl = "https://developer.mozilla.org/en-US/docs/Web/HTML/Element"
+
 export default function({ data }) {
   const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
+  const { title, contentCategories } = frontmatter
+
   return (
-    <Layout title={frontmatter.title}>
-      <SEO title={frontmatter.title} />
+    <Layout title={title}>
+      <SEO title={title} />
       <div className="blog-post">
         <h2>Content Categories</h2>
         <ul>
-          {frontmatter.contentCategories.map((category, i) => {
+          {contentCategories.map((category, i) => {
             return <li key={i}>{category}</li>
           })}
         </ul>
@@ -21,6 +25,14 @@ export default function({ data }) {
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
         />
+        <h2>Links</h2>
+        <ul>
+          <li>
+            <a href={`${mdnUrl}/${title}`} target="_blank">
+              MDN web docs
+            </a>
+          </li>
+        </ul>
       </div>
     </Layout>
   )
