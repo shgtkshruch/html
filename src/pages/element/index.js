dc7f3aec94e6b3626b2753db4e7f283d284a8816
shgtkshruch <shgtk.shruch@gmail.com>
2020-01-04 12:42:55 +0900

    Rename

--- src/pages/element/index.js
+++ src/pages/element/index.js
@@ -3,7 +3,7 @@ import React from "react"
 import { Link, StaticQuery, graphql } from "gatsby"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
-import Button from "../../components/filtering"
+import Button from "../../components/button"
 
 class ElementList extends React.Component {
   constructor() {
